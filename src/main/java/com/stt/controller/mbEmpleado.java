62060dadb5118f69155c71acdd6e9c75e1b4df93
GarySTT <gary.garcia@sttlg.us>
2021-03-16 15:03:23 -0600

    Delete

--- src/main/java/com/stt/controller/mbEmpleado.java
+++ src/main/java/com/stt/controller/mbEmpleado.java
@@ -14,40 +14,5 @@ import stt.com.entety.Empleado;
 @SessionScoped
 public class mbEmpleado implements Serializable {
 
-    @EJB
-    private EmpleadoFacade empleadofacade;
-    private List<Empleado> lstEmpleado;
-    private Empleado empleado;
-
-    public List<Empleado> getLstEmpleado() {
-        this.lstEmpleado = this.empleadofacade.findAll();
-        System.out.println("Empleado lista: " + this.empleado);
-        return lstEmpleado;
-    }
-
-    public void setLstEmpleado(List<Empleado> lstEmpleado) {
-        this.lstEmpleado = lstEmpleado;
-    }
-
-    public Empleado getEmpleado() {
-        return empleado;
-    }
-
-    public EmpleadoFacade getEmpleadofacade() {
-        return empleadofacade;
-    }
-
-    public void setEmpleadofacade(EmpleadoFacade empleadofacade) {
-        this.empleadofacade = empleadofacade;
-    }
-
-    public void setEmpleado(Empleado empleado) {
-        this.empleado = empleado;
-    }
-
-    @PostConstruct
-    public void init() {
-        this.empleado = new Empleado();
-    }
 
 }
